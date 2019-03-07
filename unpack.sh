919e7cec6a12020692336df7d79ac74ddcc2081b
Austin McCallon <aumc7782@colorado.edu>
2019-03-07 18:34:52 +0000

    unpack bash script

---
+++ unpack.sh
@@ -0,0 +1,16 @@
+
+#!/usr/bin/env bash
+CURR_DIR=/home/vagrant/work/week6/cwl-data/data/structured
+NEW_DIR=/home/vagrant/work/week7/cwl-data/data/structured
+
+#For loop to untar all tarballs in current directory
+for TARFILE in structured-2018*.tar.gz;
+do
+	 cd "$CURR_DIR"
+	mv "$TARFILE" "$NEW_DIR" 
+	cd "$NEW_DIR"
+	 tar xf "$TARFILE"
+	cd "$CURR_DIR";
+
+ done
+
